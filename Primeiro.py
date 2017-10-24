5a4275b1ab1415a6d8a08a02fe8661c896123863
luiz <luizhdinizf>
2017-10-23 22:53:42 -0200

    Reajuste de funcoes

--- Primeiro.py
+++ Primeiro.py
@@ -1,59 +1,33 @@
-"""
-==================================
-The Tipping Problem - The Hard Way
-==================================
-
- Note: This method computes everything by hand, step by step. For most people,
- the new API for fuzzy systems will be preferable. The same problem is solved
- with the new API `in this example <./plot_tipping_problem_newapi.html>`_.
-
-The 'tipping problem' is commonly used to illustrate the power of fuzzy logic
-principles to generate complex behavior from a compact, intuitive set of
-expert rules.
-
-Input variables
----------------
-
-A number of variables play into the decision about how much to tip while
-dining. Consider two of them:
-
-* ``quality`` : Quality of the food
-* ``service`` : Quality of the service
-
-Output variable
----------------
-
-The output variable is simply the tip amount, in percentage points:
-
-* ``tip`` : Percent of bill to add as tip
-
-
-For the purposes of discussion, let's say we need 'high', 'medium', and 'low'
-membership functions for both input variables and our output variable. These
-are defined in scikit-fuzzy as follows
-
-"""
 import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
-# Generate universe variables
-#   * Quality and service on subjective ranges [0, 10]
-#   * Tip has a range of [0, 25] in units of percentage points
+def soma(hX):
+   c = np.zeros(hX.shape[1])
+   for i in hX:    
+       c = c + i
+   return c
 
+
+def t_norma_min(x1,x2):
+    return np.fmin(x1,x2)
+def s_norma_max(x1,x2):
+    return np.fmax(x1,x2)
+def s_norma_sum(x1,x2):
+    c = np.add(x1,x2)
+    d = np.multiply(x1,x2)  
+    return np.subtract(c,d)
+
+
+def agreggation(x1,x2):
+    return s_norma_sum(x1,x2)
+def s_norma(x1,x2):
+    return s_norma_max(x1,x2)
+def t_norma(x1,x2):
+    return t_norma_min(x1,x2)
 def pertinencia(x,pontos):
     return  fuzz.trimf(x,pontos)
-def agreggation(x1,x2):
-    return np.fmax(x1,x2)
-def s_norma(x1,x2):
-    return np.fmax(x1,x2)
-def t_norma(x1,x2):
-    return np.fmin(x1,x2)
 
-
-x_qual = np.arange(0, 11, 1)
-x_serv = np.arange(0, 11, 1)
-x_tip  = np.arange(0, 26, 1)
 
 x = np.arange(-4, 5, 1)
 y_real = np.square(x)
@@ -100,9 +74,6 @@ ax1.plot(y, y_proximo_0, 'g', linewidth=1.5, label='0')
 
 
 
-# Now we take our rules and apply them. Rule 1 concerns bad food OR service.
-# The OR operator means we take the maximum of these two.
-#active_rule0 = np.fmax(x_proximo_0, x_proximo_menos_4)
 
 active_rule0 = x_proximo_0
 active_rule1 = s_norma(x_proximo_1, x_proximo_menos_1)
@@ -110,14 +81,16 @@ active_rule2 = s_norma(x_proximo_2, x_proximo_menos_2)
 active_rule3 = s_norma(x_proximo_3, x_proximo_menos_3)
 active_rule4 = s_norma(x_proximo_4, x_proximo_menos_4)
 
-#
-#ax2.plot(x, active_rule1, 'r', linewidth=1.5, label='16')
-#ax2.plot(x, active_rule2, 'g', linewidth=1.5, label='16')
-#ax2.plot(x, active_rule3, 'b', linewidth=1.5, label='16')
-#ax2.plot(x, active_rule4, 'r', linewidth=1.5, label='16')
+ax2.plot(x, active_rule1, 'r', linewidth=1.5, label='16')
+ax2.plot(x, active_rule2, 'g', linewidth=1.5, label='16')
+ax2.plot(x, active_rule3, 'b', linewidth=1.5, label='16')
+ax2.plot(x, active_rule4, 'r', linewidth=1.5, label='16')
+
 
 x_simu = np.arange(-4, 4, 0.01)
 y_simu = []
+
+fig, ax0 = plt.subplots(nrows=1, figsize=(8, 9))
 
 for i in x_simu:
     x_test = i
@@ -143,99 +116,13 @@ for i in x_simu:
     
     tip = fuzz.defuzz(y, saida_fuzzy, 'centroid')
     print(tip)
-    y_simu.append(tip)
-ax2.plot(y, saida_fuzzy, 'g', linewidth=1.5, label='16')
-ax2.plot(x_simu, y_simu, 'b', linewidth=1.5, label='16')
-# Now we apply this by clipping the top off the corresponding output
-# membership function with `np.fmin`
-#tip_activation_lo = np.fmin(active_rule1, tip_lo)  # removed entirely to 0
-#
-## For rule 2 we connect acceptable service to medium tipping
-#tip_activation_md = np.fmin(serv_level_md, tip_md)
-#
-## For rule 3 we connect high service OR high food with high tipping
-#active_rule3 = np.fmax(qual_level_hi, serv_level_hi)
-#tip_activation_hi = np.fmin(active_rule3, tip_hi)
-#tip0 = np.zeros_like(x_tip)
+    y_simu.append(tip)    
+#    ax1.plot(y, saida_fuzzy, 'g', linewidth=1.5, label='16')
 
-# Visualize this
-#fig, ax0 = plt.subplots(figsize=(8, 3))
 
-#ax0.fill_between(x_tip, tip0, tip_activation_lo, facecolor='b', alpha=0.7)
-#ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-#ax0.fill_between(x_tip, tip0, tip_activation_md, facecolor='g', alpha=0.7)
-#ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-#ax0.fill_between(x_tip, tip0, tip_activation_hi, facecolor='r', alpha=0.7)
-#ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-#ax0.set_title('Output membership activity')
-#
-## Turn off top/right axes
-#for ax in (ax0,):
-#    ax.spines['top'].set_visible(False)
-#    ax.spines['right'].set_visible(False)
-#    ax.get_xaxis().tick_bottom()
-#    ax.get_yaxis().tick_left()
-#
-#plt.tight_layout()
+#y2 = soma(asarray(y_simu)
+#e = y_real-y2
+#rms = np.sqrt(np.mean(e**2))
 
-"""
-.. image:: PLOT2RST.current_figure
-
-Rule aggregation
-----------------
-
-With the *activity* of each output membership function known, all output
-membership functions must be combined. This is typically done using a
-maximum operator. This step is also known as *aggregation*.
-
-Defuzzification
----------------
-Finally, to get a real world answer, we return to *crisp* logic from the
-world of fuzzy membership functions. For the purposes of this example
-the centroid method will be used.
-
-The result is a tip of **20.2%**.
----------------------------------
-"""
-
-# Aggregate all three output membership functions together
-#aggregated = np.fmax(tip_activation_lo,
-#                     np.fmax(tip_activation_md, tip_activation_hi))
-#
-## Calculate defuzzified result
-#tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
-#tip_activation = fuzz.interp_membership(x_tip, aggregated, tip)  # for plot
-
-# Visualize this
-#fig, ax0 = plt.subplots(figsize=(8, 3))
-#
-#ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
-#ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-#ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-#ax0.fill_between(x_tip, tip0, aggregated, facecolor='Orange', alpha=0.7)
-#ax0.plot([tip, tip], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
-#ax0.set_title('Aggregated membership and result (line)')
-#
-## Turn off top/right axes
-#for ax in (ax0,):
-#    ax.spines['top'].set_visible(False)
-#    ax.spines['right'].set_visible(False)
-#    ax.get_xaxis().tick_bottom()
-#    ax.get_yaxis().tick_left()
-#
-#plt.tight_layout()
-
-"""
-.. image:: PLOT2RST.current_figure
-
-Final thoughts
---------------
-
-The power of fuzzy systems is allowing complicated, intuitive behavior based
-on a sparse system of rules with minimal overhead. Note our membership
-function universes were coarse, only defined at the integers, but
-``fuzz.interp_membership`` allowed the effective resolution to increase on
-demand. This system can respond to arbitrarily small changes in inputs,
-and the processing burden is minimal.
-
-"""
+ax0.plot(x, y_real, 'b', linewidth=1.5, label='16')
+ax0.plot(x_simu, y_simu, 'r', linewidth=1.5, label='16')
