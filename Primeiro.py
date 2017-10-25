1babc4af8fb1ac45501742226013cdc8db4e6031
luiz <luizhdinizf>
2017-10-25 19:53:44 -0200

    Adicionado Função de calculo do erro RMS e incluido grade nos graficos Adicionado tambem função de pertinencia com gaussiana que usa os pontos da triangular

--- Primeiro.py
+++ Primeiro.py
@@ -2,11 +2,13 @@ import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
-def soma(hX):
-   c = np.zeros(hX.shape[1])
-   for i in hX:    
-       c = c + i
-   return c
+def erro(y_real,y_simu):
+    y2 = np.subtract(y_real,y_simu)
+    y2 = np.square(y2)
+    y2=np.sqrt(y2)
+    rms=np.sum(y2)
+    rms=rms/len(y2)
+    return rms
 
 
 def t_norma_min(x1,x2):
@@ -26,10 +28,17 @@ def s_norma(x1,x2):
 def t_norma(x1,x2):
     return t_norma_min(x1,x2)
 def pertinencia(x,pontos):
+    centro = pontos[1]
+    sigma = pontos[2]-pontos[0]
+#    return fuzz.membership.gaussmf(x,centro,sigma)
     return  fuzz.trimf(x,pontos)
+def pertinencia_gauss(x,pontos):
+    centro = pontos[1]
+    sigma = pontos[2]-pontos[0]
+    return fuzz.membership.gaussmf(x,centro,sigma)
 
 
-x = np.arange(-4, 5, 1)
+x = np.arange(-4, 4, 0.01)
 y_real = np.square(x)
 
 y = np.arange(0, 17, 1)
@@ -46,6 +55,9 @@ x_proximo_menos_3 = pertinencia(x, [-4, -3, -2])
 x_proximo_menos_4 = pertinencia(x, [-5, -4, -3])
 
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
+ax0.grid(color='000000', linestyle='dashed', linewidth=0.4)
+ax1.grid(color='000000', linestyle='dashed', linewidth=0.4)
+ax2.grid(color='000000', linestyle='dashed', linewidth=0.4)
 
 ax0.plot(x, x_proximo_4, 'r', linewidth=1.5, label='4')
 ax0.plot(x, x_proximo_3, 'g', linewidth=1.5, label='3')
@@ -59,12 +71,14 @@ ax0.plot(x, x_proximo_menos_4, 'b', linewidth=1.5, label='1')
 ax0.set_title('Entradas')
 ax0.legend()
 
-y_proximo_16 = pertinencia(y, [9, 16, 16])
+y_proximo_16 = pertinencia(y, [9, 16, 20])
 y_proximo_9 = pertinencia(y, [4, 9, 16])
 y_proximo_4 = pertinencia(y, [1, 4, 9])
 y_proximo_1 = pertinencia(y, [0, 1, 4])
 y_proximo_0 = pertinencia(y, [0, 0, 1])
 
+ax1.set_title('Saidas')
+ax1.legend()
 ax1.plot(y, y_proximo_16, 'r', linewidth=1.5, label='16')
 ax1.plot(y, y_proximo_9, 'g', linewidth=1.5, label='9')
 ax1.plot(y, y_proximo_4, 'b', linewidth=1.5, label='4')
@@ -81,6 +95,8 @@ active_rule2 = s_norma(x_proximo_2, x_proximo_menos_2)
 active_rule3 = s_norma(x_proximo_3, x_proximo_menos_3)
 active_rule4 = s_norma(x_proximo_4, x_proximo_menos_4)
 
+ax2.set_title('Regras')
+ax2.legend()
 ax2.plot(x, active_rule1, 'r', linewidth=1.5, label='16')
 ax2.plot(x, active_rule2, 'g', linewidth=1.5, label='16')
 ax2.plot(x, active_rule3, 'b', linewidth=1.5, label='16')
@@ -91,7 +107,7 @@ x_simu = np.arange(-4, 4, 0.01)
 y_simu = []
 
 fig, ax0 = plt.subplots(nrows=1, figsize=(8, 9))
-
+ax0.grid(color='000000', linestyle='dashed', linewidth=0.4)
 for i in x_simu:
     x_test = i
     
@@ -119,10 +135,7 @@ for i in x_simu:
     y_simu.append(tip)    
 #    ax1.plot(y, saida_fuzzy, 'g', linewidth=1.5, label='16')
 
-
-#y2 = soma(asarray(y_simu)
-#e = y_real-y2
-#rms = np.sqrt(np.mean(e**2))
+rms = erro(y_real,y_simu)
 
 ax0.plot(x, y_real, 'b', linewidth=1.5, label='16')
 ax0.plot(x_simu, y_simu, 'r', linewidth=1.5, label='16')
